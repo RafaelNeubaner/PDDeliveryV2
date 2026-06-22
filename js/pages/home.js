8a4f1817cbb85ffb825ea614f93cd679ad8a9fe3
PabloPerdigao <pabloagps@gmail.com>
2026-06-22 10:09:47 -0300

    feat: implementa função para carregar cardápio de produtos em oferta

---
+++ js/pages/home.js
@@ -0,0 +1,40 @@
+import { criaCardProduto } from "../components/productCard.js";
+import { useProducts } from "../services/useProducts.js";
+
+// Função para carregar o cardápio de produtos em oferta
+async function carregarCardapio() {
+  try {
+    const produtos = await useProducts.findProdutos();
+
+    const containerCardapio = document.getElementById("cardapio");
+    containerCardapio.innerHTML = "";
+
+    // limita a 15 itenspara exiibr no grid
+    const produtosEmOferta = produtos
+      .filter((produto) => produto.discount > 0)
+      .slice(0, 15);
+
+    if (produtosEmOferta.length === 0) {
+      containerCardapio.innerHTML =
+        '<p class="text-center mt-4">Nenhuma oferta disponível no momento.</p>';
+      return;
+    }
+
+    let todosCards = "";
+
+    produtosEmOferta.forEach((produto) => {
+      const cardProduto = criaCardProduto(produto);
+
+      todosCards += cardProduto;
+    });
+
+    containerCardapio.innerHTML = todosCards;
+
+  } catch (error) {
+    console.error("Busca não encontrada:", error);
+    document.getElementById("cardapio").innerHTML =
+      "<p>Erro ao carregar as ofertas.</p>";
+  }
+}
+
+carregarCardapio();
